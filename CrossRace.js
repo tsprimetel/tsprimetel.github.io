136ffe2a17e445ae03b27b8ca127c15f5f83ae1d
tsprimetel <51213400+tsprimetel@users.noreply.github.com>
2019-05-31 22:59:46 +0300

    Add files via upload

---
+++ CrossRace.js
@@ -0,0 +1,132 @@
+var AlexisMitas = 90;
+var AndreasAnastasiou = 4;
+var AndraKyprianou = 5;
+var ChristosCharalampous = 10;
+var DemetrisGeorgiou = 2;
+var EftychiaPhylactide = 6;
+var FoteinyKyriakou = 5;
+var GeorgeCharalampous = 0;
+var GeorgeLazari = 12;
+var GeorgeLouca = 14;
+var KonstantinosTsitzilonis = 3;
+var KonstantinosKosta = 7;
+var MariosParaschou = 18;
+var MichalisKyprianou = 23;
+var MichalisSkoutaris = 2;
+var PaniCharalampous = 6;
+var ParisLoizou = 18;
+var GeorgeChristodoulou = 73;
+var SteliosAngeli = 9;
+
+
+let am;
+let aa;
+let ak;
+let ccha;
+let dg;
+let ef;
+let fk;
+let gcha;
+let glou;
+let kt;
+let kk;
+let mp; 
+let mk;
+let ms;
+let pc;
+let gchr;
+let sa;
+
+
+
+function preload() {
+  // Images repo
+  
+  am = loadImage('am.png');
+  aa = loadImage('aa.png');
+  ak = loadImage('ak.png');
+  ccha = loadImage('ccha.png');
+  dg = loadImage('dg.png');
+  ef = loadImage('ef.png');
+  fk = loadImage('fk.png');
+  gcha = loadImage('gcha.png');
+  glou = loadImage('glou.png');
+  kt = loadImage('kt.png');
+  kk = loadImage('kk.png');
+  mp = loadImage('mp.png');
+  mk = loadImage('mk.png');
+  ms = loadImage('ms.png');
+  pc = loadImage('pc.png');
+  gchr = loadImage('gchr.png');
+  sa = loadImage('sa.png');
+
+
+
+
+
+}
+function setup() {
+  createCanvas(1150,1500);
+  
+  
+
+  
+  
+}
+
+function draw() {
+  background(185,0,0);
+  
+  // Agents
+  fill(255);
+  
+  image(am,AlexisMitas*10,50,50,50);
+  image(aa,AndreasAnastasiou*10,125,50,50);
+  image(ak,AndraKyprianou*10,200,50,50);
+  image(ccha,ChristosCharalampous*10,275,50,50);
+  image(dg,DemetrisGeorgiou*10,350,50,50);
+  image(ef,EftychiaPhylactide*10,425,50,50);
+  image(fk,FoteinyKyriakou*10,500,50,50);
+  image(gcha,GeorgeCharalampous*10,575,50,50);
+  rect(GeorgeLazari*10,650,50,50);
+  image(glou,GeorgeLouca*10,725,50,50);
+  image(kt,KonstantinosTsitzilonis*10,800,50,50);
+  image(kk,KonstantinosKosta*10,875,50,50);
+  image(mp,MariosParaschou*10,950,50,50);
+  image(mk,MichalisKyprianou*10,1025,50,50);
+  image(ms,MichalisSkoutaris*10,1100,50,50);
+  image(pc,PaniCharalampous*10,1175,50,50);
+  rect(ParisLoizou*10,1250,50,50);
+  image(gchr,GeorgeChristodoulou*10,1325,50,50);
+  image(sa,SteliosAngeli*10,1400,50,50);
+
+  // Lanes
+  var lanes = 50;
+  while(lanes < height){
+	  rect(-10, width,1,1);
+	  lanes = lanes +50;
+  }
+  
+  
+  
+  
+  
+  
+  // Finish line Draw
+  fill(255);
+  
+  var fnone = 0;
+  while(fnone < height){
+  rect(1000,fnone,25,25);
+    fnone = fnone + 50;
+  }
+  
+  var fntwo = 25;
+  while(fntwo < height){
+  rect(1025,fntwo,25,25);
+    fntwo = fntwo + 50;
+  }
+  
+  
+  
+}
