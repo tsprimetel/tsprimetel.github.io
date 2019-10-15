5a5a831678d6c8504fc44e0c20ccf9c8ec95d0ff
tsprimetel <51213400+tsprimetel@users.noreply.github.com>
2019-10-15 06:35:39 +0300

    Update CrossSales.js

--- CrossSales.js
+++ CrossSales.js
@@ -1,6 +1,9 @@
 
 
 
+
+
+
 let imageMariosParaschou,MariosParaschou;
 let imageDimitrisGeorgiou,DimitrisGeorgiou;
 let imageAntreasAnastasiou,AntreasAnastasiou;
@@ -28,7 +31,7 @@ let imageMichalisChatzipapa,MichalisChatzipapa;
 let img;
 let myFont;
 
-
+let arr = [];
 
 
 
@@ -64,27 +67,27 @@ function setup() {
     canvas = createCanvas(windowWidth,windowHeight); 
 
 
-    DimitrisGeorgiou = new Agents("Dimitris Georgiou",imageDimitrisGeorgiou,0,9);
-    MariosParaschou = new Agents("Marios Paraschou",imageMariosParaschou,2,6);
-    AntreasAnastasiou = new Agents("Antreas Anastasiou",imageAntreasAnastasiou,0,15);
-    AntriaKyprianou = new Agents("Antria Kyprianou",imageAntriaKyprianou,2,8);
-    EftychiaPhylactide  = new Agents("EftychiaPhylactide",imageEftychiaPhylactide,0,5);
-    FiliposKarayiannis  = new Agents("Filippos Karayiannis",imageFiliposKarayiannis,0,2);
-    GeorgeCharalampous = new Agents("George Charalampous",imageGeorgeCharalampous,0,7);
-    GeorgeChristodoulou = new Agents("George Christodoulou",imageGeorgeChristodoulou,0,4);
-    GeorgeZafeiropoulos  = new Agents("George Zafeiropoulos",imageGeorgeZafeiropoulos,0,1);
-    KonstantinosKosta = new Agents("Konstantinos Kosta",imageKonstantinosKosta,25,10);
-    KonstantinosTsitzilonis = new Agents("Konstantinos Tsitzilonis",imageKonstantinosTsitzilonis,2,11);
-    MichalisKyprianou   = new Agents("Michalis Kyprianou",imageMichalisKyprianou,6,12);
-    MichalisSkoutaris   = new Agents("Michalis Skoutaris",imageMichalisSkoutaris,1,13);
-    PanagiotaCharalampous = new Agents("Pani Charalampous",imagePanagiotaCharalampous,2,14);
-    SteliosAngeli  = new Agents("Stelios Angeli",imageSteliosAngeli,0,3);
-    GeorgeLazari = new Agents("George Lazari",imageGeorgeLazari,0,16);
-    FrixosKatsampas = new Agents("Frixos Katsampas",imageFrixosKatsampas,0,17);
-    AndreasChristodoulou = new Agents("Andreas Christodoulou",imageAndreasChristodoulou,0,18);
-    EleniSpyrou = new Agents("Eleni Spyrou",imageEleniSpyrou,0,19);
-    MariosIordanous = new Agents("Marios Iordanous",imageMariosIordanous,0,20);
-    MichalisChatzipapa  = new Agents("Michalis Chatzipapa",imageMichalisChatzipapa,0,21);
+    arr.push(new Agents("Dimitris Georgiou",imageDimitrisGeorgiou,0,9));
+    arr.push(new Agents("Marios Paraschou",imageMariosParaschou,2,6));
+    arr.push(new Agents("Antreas Anastasiou",imageAntreasAnastasiou,0,15));
+    arr.push(new Agents("Antria Kyprianou",imageAntriaKyprianou,2,8));
+    arr.push(new Agents("EftychiaPhylactide",imageEftychiaPhylactide,0,5));
+    arr.push(new Agents("Filippos Karayiannis",imageFiliposKarayiannis,0,2));
+    arr.push(new Agents("George Charalampous",imageGeorgeCharalampous,0,7));
+    arr.push(new Agents("George Christodoulou",imageGeorgeChristodoulou,0,4));
+    arr.push(new Agents("George Zafeiropoulos",imageGeorgeZafeiropoulos,0,1));
+    arr.push(new Agents("Konstantinos Kosta",imageKonstantinosKosta,25,10));
+    arr.push(new Agents("Konstantinos Tsitzilonis",imageKonstantinosTsitzilonis,2,11));
+    arr.push(new Agents("Michalis Kyprianou",imageMichalisKyprianou,6,12));
+    arr.push(new Agents("Michalis Skoutaris",imageMichalisSkoutaris,1,13));
+    arr.push(new Agents("Pani Charalampous",imagePanagiotaCharalampous,2,14));
+    arr.push(new Agents("Stelios Angeli",imageSteliosAngeli,0,3));
+    arr.push(new Agents("George Lazari",imageGeorgeLazari,0,16));
+    arr.push(new Agents("Frixos Katsampas",imageFrixosKatsampas,0,17));
+    arr.push(new Agents("Andreas Christodoulou",imageAndreasChristodoulou,0,18));
+    arr.push(new Agents("Eleni Spyrou",imageEleniSpyrou,0,19));
+    arr.push(new Agents("Marios Iordanous",imageMariosIordanous,0,20));
+    arr.push(new Agents("Michalis Chatzipapa",imageMichalisChatzipapa,0,21));
 
 }
 
@@ -97,56 +100,25 @@ function draw() {
     text("CROSS SALES",windowWidth * 0.5,windowHeight * 0.05);
     noFill();
     textAlign(LEFT);
+	for (i = 0; i <arr.length; i++){
+		arr[i].show();
+		arr[i].move();
+		
+	}
+	for (i = 0; i <arr.length; i++){
+		arr[i].hoverAgent();
+	}
 
 
-    DimitrisGeorgiou.show(); DimitrisGeorgiou.move();     
-    MariosParaschou.show(); MariosParaschou.move();  
-    AntreasAnastasiou.show();AntreasAnastasiou.move();
-    AntriaKyprianou.show(); AntriaKyprianou.move();
-    EftychiaPhylactide.show(); EftychiaPhylactide.move();
-    FiliposKarayiannis.show(); FiliposKarayiannis.move();
-    GeorgeCharalampous.show(); GeorgeCharalampous.move();
-    GeorgeChristodoulou.show(); GeorgeChristodoulou.move();
-    GeorgeZafeiropoulos.show(); GeorgeZafeiropoulos.move();
-    KonstantinosKosta.show(); KonstantinosKosta.move();
-    KonstantinosTsitzilonis.show(); KonstantinosTsitzilonis.move();
-    MichalisKyprianou.show(); MichalisKyprianou.move();
-    MichalisSkoutaris.show(); MichalisSkoutaris.move();
-    PanagiotaCharalampous.show(); PanagiotaCharalampous.move();
-    SteliosAngeli.show(); SteliosAngeli.move();
-    GeorgeLazari.show(); GeorgeLazari.move();
-    FrixosKatsampas.show(); FrixosKatsampas.move();
-    AndreasChristodoulou.show(); AndreasChristodoulou.move();
-    EleniSpyrou.show(); EleniSpyrou.move();
-    MariosIordanous.show(); MariosIordanous.move();
-    MichalisChatzipapa.show(); MichalisChatzipapa.move();
    
-    DimitrisGeorgiou.hoverAgent();
-    MariosParaschou.hoverAgent();
-    AntreasAnastasiou.hoverAgent();
-    AntriaKyprianou.hoverAgent();
-    EftychiaPhylactide.hoverAgent();
-    FiliposKarayiannis.hoverAgent();
-    GeorgeCharalampous.hoverAgent();
-    GeorgeChristodoulou.hoverAgent();
-    GeorgeZafeiropoulos.hoverAgent();
-    KonstantinosKosta.hoverAgent();
-    KonstantinosTsitzilonis.hoverAgent();
-    MichalisKyprianou.hoverAgent();
-    MichalisSkoutaris.hoverAgent();
-    PanagiotaCharalampous.hoverAgent();
-    SteliosAngeli.hoverAgent();
-    GeorgeLazari.hoverAgent();
-    FrixosKatsampas.hoverAgent();
-    AndreasChristodoulou.hoverAgent();
-    EleniSpyrou.hoverAgent();
-    MariosIordanous.hoverAgent();
-    MichalisChatzipapa.hoverAgent();
-
 }
 function windowResized() {
   canvas = createCanvas(windowWidth,windowHeight); 
  }
+ 
+ 
+ 
+
  
  
  
