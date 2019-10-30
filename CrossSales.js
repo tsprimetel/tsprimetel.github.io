9accdca2764a43f13977413df84611ab103048b7
tsprimetel <51213400+tsprimetel@users.noreply.github.com>
2019-10-30 12:54:19 +0200

    Update CrossSales.js

--- CrossSales.js
+++ CrossSales.js
@@ -68,19 +68,19 @@ function setup() {
 
 
     arr.push(new Agents("Dimitris Georgiou",imageDimitrisGeorgiou,0,9));
-    arr.push(new Agents("Marios Paraschou",imageMariosParaschou,2,6));
+    arr.push(new Agents("Marios Paraschou",imageMariosParaschou,0,6));
     arr.push(new Agents("Antreas Anastasiou",imageAntreasAnastasiou,0,15));
-    arr.push(new Agents("Antria Kyprianou",imageAntriaKyprianou,2,8));
+    arr.push(new Agents("Antria Kyprianou",imageAntriaKyprianou,0,8));
     arr.push(new Agents("EftychiaPhylactide",imageEftychiaPhylactide,0,5));
     arr.push(new Agents("Filippos Karayiannis",imageFiliposKarayiannis,0,2));
     arr.push(new Agents("George Charalampous",imageGeorgeCharalampous,0,7));
     arr.push(new Agents("George Christodoulou",imageGeorgeChristodoulou,0,4));
     arr.push(new Agents("George Zafeiropoulos",imageGeorgeZafeiropoulos,0,1));
-    arr.push(new Agents("Konstantinos Kosta",imageKonstantinosKosta,25,10));
-    arr.push(new Agents("Konstantinos Tsitzilonis",imageKonstantinosTsitzilonis,2,11));
-    arr.push(new Agents("Michalis Kyprianou",imageMichalisKyprianou,6,12));
-    arr.push(new Agents("Michalis Skoutaris",imageMichalisSkoutaris,1,13));
-    arr.push(new Agents("Pani Charalampous",imagePanagiotaCharalampous,2,14));
+    arr.push(new Agents("Konstantinos Kosta",imageKonstantinosKosta,4,10));
+    arr.push(new Agents("Konstantinos Tsitzilonis",imageKonstantinosTsitzilonis,0,11));
+    arr.push(new Agents("Michalis Kyprianou",imageMichalisKyprianou,0,12));
+    arr.push(new Agents("Michalis Skoutaris",imageMichalisSkoutaris,0,13));
+    arr.push(new Agents("Pani Charalampous",imagePanagiotaCharalampous,0,14));
     arr.push(new Agents("Stelios Angeli",imageSteliosAngeli,0,3));
     arr.push(new Agents("George Lazari",imageGeorgeLazari,0,16));
     arr.push(new Agents("Frixos Katsampas",imageFrixosKatsampas,0,17));
